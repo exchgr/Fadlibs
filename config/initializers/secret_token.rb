0b6c355bab9a68cdfd633abec8fd9da93dab530d
Dan Mundy <drm.mundy@gmail.com>
2012-09-29 22:13:13 -0400

    Sets up a blank Rails app with haml, coffee, and scss assets, as well as zurb foundation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fadlibs::Application.config.secret_token = '27d5202105790a174642812453495d6d226b6ba6bb420eceeb8d539c355f762ec07fadecc7fd66fa5958bcb0a9c3cf9e479c32c7cee25da32513f7077f6348d2'
